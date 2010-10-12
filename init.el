b54d21cd443c7d046c7bb4a7425d52609186c0bb
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2010-10-12 13:52:51 +0200

    Gemfile is a ruby file

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
 (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
